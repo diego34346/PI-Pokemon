0d93573b69a8aa4c8ae7a0e66a2a0a21ba0140df
diego34346 <diego_f423@hotmail.com>
2023-07-17 20:35:30 -0500

    Models-relacion_models

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,51 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
+
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    image:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    hp:{
+      type: DataTypes.INTEGER,
+    },
+
+    attack:{
+      type: DataTypes.INTEGER,
+    },
+
+    defense:{
+      type: DataTypes.INTEGER,
+    },
+
+    speed:{
+      type: DataTypes.INTEGER,
+    },
+
+    height:{
+      type: DataTypes.INTEGER,
+    },
+
+    weight:{
+      type: DataTypes.INTEGER,
+    },
+
+    type:{
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      defaultValue: []
+    }
+    
   });
 };
